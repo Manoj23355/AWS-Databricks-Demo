8c18089d1c9fe21c3ec4e0231b0687a693840a6f
Manoj23355 <manoj.funny.varma@gmail.com>
2025-03-25 16:00:07 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,4 @@ resource "aws_databricks_workspace" "db_workspace" {
  workspace_name = var.workspace_name
  pricing_tier = var.pricing_tier
 }
-output "workspace_url" {
- value = aws_databricks_workspace.db_workspace.workspace_url
-}
+
